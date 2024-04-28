8404c4c7da353ef8eba29a2c7b27ff9c5a001374
patadaoikkk <delgm2020@gmail.com>
2024-04-28 18:27:30 -0300

    alterações

---
+++ src/test/java/br/edu/eteczl/catracasbackend/CatracasbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.eteczl.catracasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatracasbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
